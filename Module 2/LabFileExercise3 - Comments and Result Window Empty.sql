f5293135078741b7374ca995cbf9c21e3719f9df
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2021-01-22 17:09:41 -0300

    Module 2 - LabFileExercise3 - Comments and Result Window Empty

---
+++ Module 2/LabFileExercise3 - Comments and Result Window Empty.sql
@@ -0,0 +1,54 @@
+/*
+---------------------------------------------------------------------
+	Module 2
+
+	Exercise 3
+*/
+
+--USE TSQL;
+--GO
+
+/*
+---------------------------------------------------------------------
+	>> Task 1
+
+	Observe the results.
+	Why is the result window empty?
+*/
+
+/*
+SELECT firstName,
+	lastName,
+	city,
+	country
+FROM HR.Employees
+WHERE country = 'USA'
+ORDER BY lastName;
+GO
+*/
+
+/*
+---------------------------------------------------------------------
+	TASK 2
+
+	Observe that before the USE statement there are the characters which means
+	that the USE statement is treated as a comment.
+	There is also a block comment around the whole T-SQL SELECT statement.
+	Uncomment both statements.
+	First execute the USE statement and then execute the statement starting with SELECT clause.
+	Observe the results.
+	Notice that the results have the same rows as in exercise 1, Task 2,
+	but they are sorted by the lastname column.
+*/
+
+USE TSQL;
+GO
+
+SELECT firstname,
+	lastname,
+	city,
+	country
+FROM HR.Employees
+WHERE country = 'USA'
+ORDER BY lastname;
+GO
