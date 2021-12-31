e3b1c47d6ed51a77afd6191c20bc80a715047f6c
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2021-12-31 08:32:43 +0100

    Module 12 - DemoFileDemonstrationA - UNION and UNION ALL

---
+++ Module 12/DemoFileDemonstrationA - UNION and UNION ALL.sql
@@ -0,0 +1,35 @@
+-- Demonstration A
+
+-- Step 1: Open a new query window to the TSQL database.
+
+USE TSQL;
+GO
+
+/*
+	Step 2: Using UNION ALL. Select this query and execute it to show the use of UNION ALL to return all rows from both tables
+	including duplicates.
+*/
+
+SELECT
+	country, region, city
+FROM HR.Employees
+
+UNION ALL
+
+SELECT
+	country, region, city
+FROM Sales.Customers;
+GO
+
+-- Step 3: Using UNION. Select this query and execute it to show the use of UNION to return all rows from both tables excluding duplicates.
+
+SELECT
+	country, region, city
+FROM HR.Employees
+
+UNION
+
+SELECT
+	country, region, city
+FROM Sales.Customers;
+GO
