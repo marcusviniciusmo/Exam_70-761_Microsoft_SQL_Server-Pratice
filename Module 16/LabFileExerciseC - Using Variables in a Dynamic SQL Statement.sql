f823b28f42e378cdf42cbd4e7b7fdc85db78c0ab
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-01-15 20:58:03 +0100

    Module 16 - LabFileExerciseC - Using Variables in a Dynamic SQL Statement

---
+++ Module 16/LabFileExerciseC - Using Variables in a Dynamic SQL Statement.sql
@@ -0,0 +1,52 @@
+---------------------------------------------------------------------
+-- LAB 16
+--
+-- Exercise 3
+---------------------------------------------------------------------
+
+USE TSQL;
+GO
+
+/*
+---------------------------------------------------------------------
+	>> TASK 1
+
+	Write a T-SQL code that defines the variable @SQLstr as nvarchar(200) data type. Set the value of the variable to a SELECT statement that
+	retrieves the empid, firstname, and lastname columns in the HR.Employees table.
+	Write an EXECUTE statement to invoke the written dynamic SQL statement inside the @SQLstr variable. Execute the T-SQL code.
+*/
+
+DECLARE @SQLstr NVARCHAR(200);
+
+SET @SQLstr = N'
+	SELECT
+		empid, firstname, lastname
+	FROM HR.Employees';
+
+EXECUTE sys.sp_executesql @statement = @SQLstr;
+GO
+
+/*
+---------------------------------------------------------------------
+	>> TASK 2
+
+	Copy the previous T-SQL code and modify it to include in the dynamic batch stored in @SQLstr, a filter in which empid is equal to a parameter
+	named @empid. In the calling batch, define a variable named @SQLparam as nvarchar(100). This variable will hold the definition of the @empid
+	parameter. This means setting the value of the @SQLparam variable to @empid int.
+	Write an EXECUTE statement that uses sp_executesql to invoke the code in the @SQLstr variable, passing the parameter definition stored in the
+	@SQLparam variable to sp_executesql. Assign the value 5 to the @empid parameter in the current execution.
+	Observe and compare the results that you got with the recommended result shown in the file 73 - Lab Exercise 3 - Task 2 Result.txt.
+*/
+
+DECLARE @SQLstr NVARCHAR(200),
+	@SQLparam NVARCHAR(100);
+
+SET @SQLstr = N'
+	SELECT
+		empid, firstname, lastname
+	FROM HR.Employees
+	WHERE empid = @empid';
+
+SET @SQLparam = N'@empid INT'
+
+EXECUTE sys.sp_executesql @statement = @SQLstr, @params = @SQLparam, @empid = 5;
